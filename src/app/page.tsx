48ac264ac48bc1d4c2fa1ea8a24cb47c82f81413
StivenF0 <stivenfonseca@gmail.com>
2023-06-11 14:32:40 -0300

    Added the gradient to the background and the emoji

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,34 @@
-import TypewritterTitle from "@/components/TypewriterTitle";
+import TypewriterTitle from "@/components/TypewriterTitle";
+import colors from "tailwindcss/colors";
 
 export default function Home() {
+  const gradient = ` 
+    radial-gradient(${colors.emerald[100]} 30%, ${colors.emerald[100]} 60%, transparent),
+    repeating-linear-gradient(
+      to right,
+      transparent,
+      transparent 149px,
+      ${colors.gray[300]} 151px,
+      ${colors.gray[300]} 151px
+    ),
+    repeating-linear-gradient(
+      to bottom,
+      transparent,
+      transparent 149px,
+      ${colors.gray[300]} 151px,
+      ${colors.gray[300]} 151px
+    )
+  `;
+
   return (
     <main>
-      <section className="bg-slate-200 w-full h-screen grid place-content-center text-4xl font-bold text-slate-900">
-        <div className="">Olá, sou Stiven!</div>
+      <section
+        className="w-full h-screen grid place-content-center text-5xl font-bold text-slate-900 bg-gradient-radial"
+        style={{ background: gradient }}
+      >
+        <div className="">Olá, sou Stiven! 👋</div>
         <div className="">
-          Programador{" "}
-          <TypewritterTitle
-            className="w-32"
-            words={["Frontend", "Backend", "Fullstack"]}
-          />
+          Programador <TypewriterTitle />
         </div>
       </section>
     </main>
--- src/components/TypewriterTitle.tsx
+++ src/components/TypewriterTitle.tsx
@@ -1,21 +1,36 @@
 "use client";
-import React from "react";
-import { useTypewriter } from "react-simple-typewriter";
+import { useRef, useState } from "react";
+import { Typewriter } from "react-simple-typewriter";
 
-function TypewritterTitle({
-  words,
-  className,
-}: {
-  words: string[];
-  className?: string;
-}) {
-  const [text] = useTypewriter({
-    words,
-    typeSpeed: 150,
-    loop: 1,
-  });
+function TypewriterTitle() {
+  const colors = [
+    "#fbbf24", // yellow
+    "#0ea5e9", // blue
+    "#059669", // green
+  ];
+  const [color, setColor] = useState(colors[0]);
+  const counter = useRef(0);
 
-  return <span className={className}>{text}</span>;
+  const handleChangeColor = () => {
+    if (counter.current >= 14) {
+      setColor(colors[2]);
+    } else if (counter.current >= 7) {
+      setColor(colors[1]);
+    }
+    counter.current++;
+  };
+
+  return (
+    <div className="inline-block w-[15.2rem]" style={{ color }}>
+      <Typewriter
+        words={["Frontend", "Backend", "Fullstack"]}
+        loop={1}
+        cursor={true}
+        onDelete={handleChangeColor}
+        delaySpeed={3000}
+      />
+    </div>
+  );
 }
 
-export default TypewritterTitle;
+export default TypewriterTitle;
